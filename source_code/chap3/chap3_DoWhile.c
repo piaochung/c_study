626fa18e6b48d85f1f237d8e02176f2db76e150a
chungchung <hangjastar@gmail.com>
2021-03-27 13:42:39 +0000

    make chap2 and chap3 data

---
+++ source_code/chap3/chap3_DoWhile.c
@@ -0,0 +1,21 @@
+#include <stdio.g>
+
+int main() {
+  int i = 0;
+  
+  printf("\ndo~while문을 이용하여 0~9까지 출력\n");
+  
+  do{
+    printf("%d\n", i);
+    i++;
+  }while(i < 10);
+
+  printf("\ndo~while문의 특징\n");
+
+  do{
+    printf("%d\n", i);
+    i++;
+  }while(i < 0);
+  
+  return 0;
+}
---
+++ source_code/chap3/chap3_While.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+  int i = 0;
+  
+  printf("\nwhile문을 이용하여 0~9까지 출력\n");
+  
+  while(i < 10){
+    printf("%d\n", i);
+    i++;
+  }
+}
